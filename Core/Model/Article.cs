0e02e85f18bca480a6baec2031f9225141f77850
huosw.fnst <huosw.fnst@G08FNSTD160035>
2017-04-06 15:55:03 +0800

    Add project files.

---
+++ Core/Model/Article.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Model
+{
+  class Article
+  {
+    private string title;
+    private List<Chapter> content;
+  }
+}
---
+++ Core/Model/Chapter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Model
+{
+  class Chapter
+  {
+    private string title;
+    private int paraSize;
+    private List<Paragraph> content;
+    private string background;
+  }
+}
